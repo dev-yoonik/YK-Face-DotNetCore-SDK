49de376b543e5116134b6a682badb9c4bd66bb3a
João Ferreira <jppdpf@outlook.pt>
2021-09-24 18:08:21 +0100

    Deleted unnecessary usings.

--- YooniK.Face/YooniK.Face.Client.Tests/Cases/Valid.cs
+++ YooniK.Face/YooniK.Face.Client.Tests/Cases/Valid.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YooniK.Face.Client.Models.Requests;
-
+﻿
 namespace YooniK.Face.DataExamples
 {
 
